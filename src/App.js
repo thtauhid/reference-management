b507be33581bed22bf14c2104dbbdfff2f05122a
Tauhid <thtauhid.71@gmail.com>
2019-08-08 03:57:49 +0600

    cleanup.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import AddNewDonor from './components/AddNewDonor'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+    		<Router>
+    			<Route path="/addNewDonor" component={AddNewDonor} />	
+    		</Router>
+    	</div>
+    );
 }
 
-export default App;
+export default App
