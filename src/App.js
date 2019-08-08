cfe6c0671dae3d7d0b121cea2e6c19a47e2a22ed
Tauhid <thtauhid.71@gmail.com>
2019-08-08 21:06:08 +0600

    ViewExistingDonor

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import AddNewDonor from './components/AddNewDonor'
+import ViewExistingDonors from './components/ViewExistingDonors'
 
 function App() {
     return (
         <div className="container">
     		<Router>
     			<Route path="/addNewDonor" component={AddNewDonor} />	
+    			<Route path="/viewExistingDonors" component={ViewExistingDonors} />	
     		</Router>
     	</div>
     );
---
+++ src/components/ViewExistingDonors.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+const Donor = props => (
+    <tr>
+		<td>{props.data.donorid}</td>
+		<td>{props.data.bloodgroup}</td>
+		<td>{props.data.donorname}</td>
+		<td>{props.data.contactno}</td>
+		<td>{props.data.location}</td>
+		<td>{props.data.lastdonation}</td>
+	</tr>
+)
+export default class ViewExistingDonors extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            donorData: []
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4500/viewExistingDonors')
+            .then(response => {
+                this.setState({
+                    donorData: response.data
+                })
+            })
+            .catch(err => console.log('Error' + err))
+    }
+
+    DonorList() {
+        return this.state.donorData.map(data => {
+            return <Donor data={data} key={data.donorid} />
+        })
+    }
+
+    render() {
+        return (
+            <div>
+				<table className="table table-striped table-bordered table-hover">
+					<thead className="thead-dark">
+						<tr>
+							<th scope="col">Donor ID</th>
+							<th scope="col">Blood Group</th>
+							<th scope="col">Donor Name</th>
+							<th scope="col">Contact No</th>
+							<th scope="col">Location</th>
+							<th scope="col">Last Donation</th>
+						</tr>
+					</thead>
+					<tbody className>
+						{this.DonorList()}
+					</tbody>
+				</table>
+			</div>
+        )
+    }
+}
