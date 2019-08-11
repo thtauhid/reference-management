5971eab10512116eb99e713794efbb5799c54d1c
Tauhid <thtauhid.71@gmail.com>
2019-08-12 00:05:38 +0600

    changing port.

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 14001
 
 app.use(cors())
 app.use(express.json())
